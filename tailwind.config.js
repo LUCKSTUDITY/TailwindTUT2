1dee9b179c80b9d10d4fa19a1c9bcef88e900af7
LUCKSTUDITY <sinhastudent10@gmail.com>
2022-03-16 10:09:12 +0530

    Second Commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily: {
+      'Times': ["Times New Roman", 'Times', 'serif'],
+      'Baloo Bhai 2': ['Baloo Bhai 2', 'cursive']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
